faa67f1fd54c839fa835ad049cc00deaf5113d2e
Varadhuu <131642420+Varadhuu@users.noreply.github.com>
2024-10-10 11:16:13 +0530

    files to upload

---
+++ backend/dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+export default async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannot  connect");
+    }
+}
+async function disconnectFromDatabase() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannor connect");
+    }
+}
+export { connectToDatabase, disconnectFromDatabase };
+//# sourceMappingURL=connection.js.map
